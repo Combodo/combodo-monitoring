6a1ae02ac89e602ff6a407952673f92d4267a964
odain <olivier.dain@combodo.com>
2021-04-09 17:30:19 +0200

    composer deps

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '753bf85ccb8ee067f1ca2286c656a81106bc19aa',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '753bf85ccb8ee067f1ca2286c656a81106bc19aa',
+    ),
+  ),
+);
