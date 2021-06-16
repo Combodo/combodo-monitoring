d7990686b601df0a80a0f3279bcfbdf256c12f7d
odain <olivier.dain@combodo.com>
2021-06-16 02:00:37 +0200

    pass some property to protected to ease MetricReader heritage + add itop version metric readers

---
+++ test/CustomReader/ItopSetupVersionReaderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Combodo\iTop\Monitoring\Test\CustomReader;
+
+use Combodo\iTop\Monitoring\CustomReader\ItopSetupVersionReader;
+use Combodo\iTop\Monitoring\Model\MonitoringMetric;
+use Combodo\iTop\Test\UnitTest\ItopDataTestCase;
+
+class ItopSetupVersionReaderTest extends ItopDataTestCase {
+
+    public function setUp()
+	{
+		//@include_once '/home/combodo/workspace/iTop/approot.inc.php';
+		//@include_once '/home/nono/PhpstormProjects/iTop/approot.inc.php';
+		parent::setUp();
+
+		require_once(APPROOT . 'env-production/combodo-monitoring/vendor/autoload.php');
+		require_once(APPROOT . 'core/config.class.inc.php');
+	}
+
+	/**
+	 * @dataProvider GetItopVersionIdProvider
+	 */
+	public function testGetItopVersionId(string $sInitialValue, string $sExpectedReturnedValue){
+    	$oItopSetupVersionReader = new ItopSetupVersionReader('', []);
+
+    	$oMetric = new MonitoringMetric('toto', '', $sInitialValue);
+		$this->assertEquals($sExpectedReturnedValue,
+			$oItopSetupVersionReader->GetItopShortVersionId($oMetric)
+		);
+	}
+
+	public function GetItopVersionIdProvider(){
+		return [
+			'no version found' => ['3.0.0-dev-svn', '0' ],
+			'no version found bis' => ['bla', '0' ],
+			'version found' => [ '3.0.0-dev-6517', '6517']
+		];
+	}
+}
