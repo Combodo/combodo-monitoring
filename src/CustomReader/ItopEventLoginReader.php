0f05edc39135fa3ec17aaac33450240043003a28
odain <olivier.dain@combodo.com>
2022-02-11 10:45:20 +0100

    add ItopEventLogin metric

---
+++ src/CustomReader/ItopEventLoginReader.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Copyright (C) 2013-2021 Combodo SARL
+ * This file is part of iTop.
+ * iTop is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * iTop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ */
+
+namespace Combodo\iTop\Monitoring\CustomReader;
+
+use Combodo\iTop\Monitoring\MetricReader\CustomReaderInterface;
+use Combodo\iTop\Monitoring\Model\Constants;
+use Combodo\iTop\Monitoring\Model\MonitoringMetric;
+
+class ItopEventLoginReader implements CustomReaderInterface
+{
+    private $aMetricConf;
+    private $sMetricName;
+    private array $aProfiles;
+
+    public function __construct($sMetricName, $aMetricConf)
+    {
+        $this->aMetricConf = $aMetricConf;
+        $this->sMetricName = 'itop_eventlogin';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function GetMetrics($sDir = null): ?array
+    {
+        $aMetrics = [];
+
+        if (array_key_exists(Constants::METRIC_LABEL, $this->aMetricConf)) {
+            $aLabels = $this->aMetricConf[Constants::METRIC_LABEL];
+        } else {
+            $aLabels = [];
+        }
+
+        foreach ($this->ListEventLogin() as $aEventLoginMetric) {
+            $aCurrentLabels = array_merge([], $aLabels);
+            $aCurrentLabels['profiles'] = $aEventLoginMetric['labels']['profiles'];
+            $aCurrentLabels['account_type'] = $aEventLoginMetric['labels']['account_type'];
+
+            $aMetrics[] = new MonitoringMetric($this->sMetricName,
+                'nb of connection in the last 1 hour.',
+                $aEventLoginMetric['count'],
+                $aCurrentLabels
+            );
+        }
+
+        return $aMetrics;
+    }
+
+    public function ListEventLogin(): array
+    {
+        $sOql = <<<OQL
+SELECT u,e FROM EventLoginUsage AS e JOIN User AS u ON e.user_id=u.id
+WHERE e.date> DATE_SUB(NOW(), INTERVAL 60 MINUTE)
+OQL;
+
+        $oSearch = \DBObjectSearch::FromOQL($sOql);
+        $oSet = new \DBObjectSet($oSearch);
+
+        $aRes = [];
+
+        /* var DBObject $oObject  */
+        while ($aObjects = $oSet->FetchAssoc()) {
+            $sProfiles = null;
+            $oUser = $aObjects['u'];
+
+            $oProfileSet = $oUser->Get('profile_list');
+            while ($oProfile = $oProfileSet->Fetch()) {
+                $sProfile = str_replace(" ","_",strtolower($oProfile->Get('profile')));
+                $sProfiles = (is_null($sProfiles)) ? $sProfile : "$sProfiles+$sProfile";
+            }
+
+            $sAccountType = strtolower(get_class($oUser));
+            $sKey = "${sProfiles}_$sAccountType";
+            if (isset($aRes[$sKey])) {
+                $aRes[$sKey]['count'] = $aRes[$sKey]['count'] + 1;
+            } else {
+                $aRes[$sKey] = [
+                    'count' => 1,
+                    'labels' => [
+                        'profiles' => $sProfiles,
+                        'account_type' => $sAccountType,
+                    ],
+                ];
+            }
+        }
+
+        return $aRes;
+    }
+}
---
+++ test/CustomReader/ItopEventLoginReaderTest.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Combodo\iTop\Test\UnitTest;
+
+use AttributeDateTime;
+use Combodo\iTop\Monitoring\CustomReader\ItopEventLoginReader;
+
+/**
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ * @backupGlobals disabled
+ */
+class ItopArchiveReaderTest extends ItopDataTestCase
+{
+    const USE_TRANSACTION = false;
+    private array $aProfiles = [];
+    private array $aUsers = [];
+
+    public function setUp()
+    {
+        //@include_once '/home/combodo/workspaceSAAS/iTopSaasManager/approot.inc.php';
+        parent::setUp();
+
+        $this->CreateTestOrganization();
+        $this->readProfiles();
+    }
+
+    private function readProfiles()
+    {
+        $oSearch = \DBObjectSearch::FromOQL('SELECT URP_Profiles');
+        $oSet = new \DBObjectSet($oSearch);
+
+        /* var \URP_Profiles $oObject  */
+        while ($aObject = $oSet->Fetch()) {
+            $sName = $aObject->Get('name');
+            $this->aProfiles[$sName] = $aObject->GetKey();
+        }
+    }
+
+    public function GetMetricsProvider()
+    {
+        return [
+            'test one eventlogin per user' => [
+                'users' => [
+                    'user1' => ['login' => 'login1', 'profiles' => ['Administrator']],
+                    'user2' => ['login' => 'login2', 'profiles' => ['Portal user']],
+                    'user3' => ['login' => 'login3', 'profiles' => ['Service Desk Agent', 'Portal power user']],
+                    'user4' => ['login' => 'login4', 'profiles' => ['Portal power user', 'Service Desk Agent']],
+                ],
+                'event_login' => [
+                    ['user' => 'user1', 'recent' => true],
+                    ['user' => 'user2', 'recent' => true],
+                    ['user' => 'user3', 'recent' => true],
+                    ['user' => 'user4', 'recent' => true],
+                ],
+                'expected_metrics' => [
+                    ['metric_name' => 'itop_eventlogin', 'metric_value' => 1, 'account_type' => 'userlocal',  'profiles' => 'administrator'],
+                    ['metric_name' => 'itop_eventlogin', 'metric_value' => 1, 'account_type' => 'userlocal',  'profiles' => 'portal_user'],
+                    ['metric_name' => 'itop_eventlogin', 'metric_value' => 2, 'account_type' => 'userlocal',  'profiles' => 'portal_power_user+service_desk_agent'],
+                ],
+            ],
+            'test eventlogin in the last hour only' => [
+                'users' => [
+                    'user1' => ['login' => 'login1', 'profiles' => ['Administrator']],
+                ],
+                'event_login' => [
+                    ['user' => 'user1', 'recent' => false],
+                    ['user' => 'user1', 'recent' => true],
+                    ['user' => 'user1', 'recent' => false],
+                ],
+                'expected_metrics' => [
+                    ['metric_name' => 'itop_eventlogin', 'metric_value' => 1, 'account_type' => 'userlocal',  'profiles' => 'administrator'],
+                ],
+            ],
+        ];
+    }
+
+    private function CreateUserWithProfiles($sLogin, $aUserProfiles)
+    {
+        $sPassword = '123456789@AbCdE';
+        $oSet = null;
+
+        $oPerson = $this->CreatePerson($sLogin);
+
+        foreach ($aUserProfiles as $sProfileName) {
+            $oUserProfile = new \URP_UserProfile();
+            $oUserProfile->Set('profileid', $this->aProfiles[$sProfileName]);
+            $oUserProfile->Set('reason', 'UNIT Tests');
+
+            if (is_null($oSet)) {
+                $oSet = \DBObjectSet::FromObject($oUserProfile);
+            } else {
+                $oSet->AddObject($oUserProfile);
+            }
+        }
+
+        $oUser = $this->createObject('UserLocal', [
+            'contactid' => $oPerson->GetKey(),
+            'login' => $sLogin,
+            'password' => $sPassword,
+            'language' => 'EN US',
+            'profile_list' => $oSet,
+        ]);
+        $this->debug("Created {$oUser->GetName()} ({$oUser->GetKey()})");
+
+        return $oUser;
+    }
+
+    /**
+     * @dataProvider GetMetricsProvider
+     */
+    public function testGetMetrics($aUsers, $aEventLogins, $aExpectedMetrics)
+    {
+        foreach ($aUsers as $sUserId => $aUser) {
+            $sLogin = $aUser['login'];
+            $aUserProfiles = $aUser['profiles'];
+            $oUser = $this->CreateUserWithProfiles($sLogin, $aUserProfiles, '123456789#123');
+            $this->aUsers[$sUserId] = $oUser;
+        }
+
+        foreach ($aEventLogins as $aEventLogin) {
+            $oUser = $this->aUsers[$aEventLogin['user']];
+            $oEventLoginObject = $this->CreateObject('EventLoginUsage', [
+                'date' => date(AttributeDateTime::GetFormat()),
+                'userinfo' => $oUser,
+                'user_id' => $oUser->GetKey(),
+                'message' => 'Successful login',
+            ]);
+
+            if (!$aEventLogin['recent']) {
+                $oEventLoginObject->Set('date', date(AttributeDateTime::GetFormat(), strtotime('-2 HOURS')));
+                $oEventLoginObject->DBWrite();
+            }
+        }
+
+        $aLabels = ['toto' => 'titi'];
+        $oItopEventLoginReader = new ItopEventLoginReader('', ['static_labels' => $aLabels]);
+
+        $aMetrics = $oItopEventLoginReader->GetMetrics();
+        $sizeof = sizeof($aExpectedMetrics);
+        var_dump($aExpectedMetrics);
+        $this->assertEquals($sizeof, sizeof($aMetrics));
+        for ($i = 0; $i < $sizeof; ++$i) {
+            /* @var \Combodo\iTop\Monitoring\Model\MonitoringMetric $oMetric */
+            $oMetric = $aMetrics[$i];
+
+            $aExpectedMetric = $aExpectedMetrics[$i];
+
+            $this->assertEquals($aExpectedMetric['metric_name'], $oMetric->GetName());
+            $this->assertEquals($aExpectedMetric['metric_value'], $oMetric->GetValue());
+            $aLabels = $oMetric->GetLabels();
+            $this->assertEquals($aExpectedMetric['account_type'], $aLabels['account_type']);
+            $this->assertEquals($aExpectedMetric['profiles'], $aLabels['profiles']);
+        }
+    }
+}
